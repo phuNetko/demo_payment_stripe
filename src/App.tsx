106ba0589e5fbcd9851b8effa2b038a9788d4e6a
PhuNetko <phu.phung@netko-solution.com>
2026-02-02 15:28:20 +0700

    add lift id to setup liff line

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { useEffect } from "react";
+import { setLoading } from "./store/themeConfigSlice";
+import { useDispatch } from "react-redux";
+import liff from "@line/liff";
+
+async function initLiff() {
+  await liff.init({
+    liffId: import.meta.env.VITE_LIFF_ID,
+  });
+
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+}
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setLoading(true))
+    initLiff()
+      .then((profile) => {
+        console.log("LINE profile:", profile);
+      })
+      .finally(() => {
+        dispatch(setLoading(false))
+      });
+  }, []);
+
   return <RouterProvider router={router} />
 }
 
